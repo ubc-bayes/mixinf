38136db9c387d81a9b227e57bf87da4663cc1e46
GiankDiluvi <giankdiluvi@gmail.com>
2021-05-27 17:12:49 -0700

    minor ed to rbf kernel

--- tests/RKHSkernels/rbf_median.py
+++ tests/RKHSkernels/rbf_median.py
@@ -18,11 +18,11 @@ def get_kernel(x):
     # t1,t2 are always (N,K) arrays
 
     # define rbf kernel
-    def kernel(t1, t2): return np.exp(-0.5*np.sum((t1[np.newaxis,:,:] - t2[:,np.newaxis,:])**2, axis = -1)/k_gamma) # returns (N,N)
+    def kernel(t1, t2): return np.exp(-0.5*np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1)/k_gamma) # returns (N,N)
 
     # derivatives of the kernel
-    def dk_x(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t1[np.newaxis,:,:] - t2[:,np.newaxis,:]) # returns (N,N,K)
-    def dk_y(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t2[np.newaxis,:,:] - t1[:,np.newaxis,:]) # returns (N,N,K)
-    def dk_xy(t1, t2): return kernel(t1,t2) * (1 - np.sum((t1[np.newaxis,:,:] - t2[:,np.newaxis,:])**2, axis = -1)/k_gamma) / k_gamma # returns (N,N)
+    def dk_x(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t1[:,np.newaxis,:] - t2[np.newaxis,:,:]) # returns (N,N,K)
+    def dk_y(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t2[:,np.newaxis,:] - t1[np.newaxis,:,:]) # returns (N,N,K)
+    def dk_xy(t1, t2): return kernel(t1,t2) * (1 - np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1)/k_gamma) / k_gamma # returns (N,N)
 
     return kernel, dk_x, dk_y, dk_xy
