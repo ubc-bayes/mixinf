9007580736ec674c6e1ee9bf024b3bc50b6dcbec
GiankDiluvi <giankdiluvi@gmail.com>
2021-06-24 15:30:30 -0700

    fix median rkhs kernel bug

--- tests/RKHSkernels/rbf_median.py
+++ tests/RKHSkernels/rbf_median.py
@@ -8,7 +8,7 @@ from autograd import grad
 # banwidth of kernel
 def get_gamma(x):
     # receives sample of size (N,K) and obtains median of squared distances between all samples
-    return np.median(np.sum((x[:,np.newaxis,:] - x[np.newaxis,:,:])**2, axis=-1))
+    return np.median(np.sum(x[:,np.newaxis,:] - x[np.newaxis,:,:], axis=-1))
 
 # kernel and first and second order derivatives
 def get_kernel(x):
@@ -18,11 +18,11 @@ def get_kernel(x):
     # t1,t2 are always (N,K) arrays
 
     # define rbf kernel
-    def kernel(t1, t2): return np.exp(-0.5*np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1)/k_gamma) # returns (N,N)
+    def kernel(t1, t2): return np.exp(-0.5*np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1)/k_gamma**2) # returns (N,N)
 
     # derivatives of the kernel
-    def dk_x(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t1[:,np.newaxis,:] - t2[np.newaxis,:,:]) # returns (N,N,K)
-    def dk_y(t1, t2): return -kernel(t1,t2)[:,:,np.newaxis] * (t2[:,np.newaxis,:] - t1[np.newaxis,:,:]) # returns (N,N,K)
-    def dk_xy(t1, t2): return kernel(t1,t2) * (1 - np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1)/k_gamma) / k_gamma # returns (N,N)
+    def dk_x(t1, t2): return kernel(t1,t2)[:,:,np.newaxis] * (t2[:,np.newaxis,:] - t1[np.newaxis,:,:]) / k_gamma**2 # returns (N,N,K)
+    def dk_y(t1, t2): return kernel(t1,t2)[:,:,np.newaxis] * (t1[:,np.newaxis,:] - t2[np.newaxis,:,:]) / k_gamma**2 # returns (N,N,K)
+    def dk_xy(t1, t2): return kernel(t1,t2) * (1 - np.sum((t1[:,np.newaxis,:] - t2[np.newaxis,:,:])**2, axis = -1) / k_gamma**2) / k_gamma**2 # returns (N,N)
 
     return kernel, dk_x, dk_y, dk_xy
