f30ca6e9a1be26bb50b4cbe4f73ab3657debea93
GiankDiluvi <giankdiluvi@gmail.com>
2021-07-22 10:40:59 -0700

    add cauchy results from arc

--- tests/cauchy1d/plot.sh
+++ tests/cauchy1d/plot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-python3 -W ignore ../univariate-plot.py --inpath 'results/' --outpath 'results/plots/' --target 'cauchy' --reps 30 --tol 0.0 --no_dens --lbvi --kernel 'gaussian' --rkhs 'rbf' --gamma 0.5 --ubvi --bvi --gvi  --rwmh
-#--hmc
+python3 -W ignore ../univariate-plot.py --inpath 'results/' --outpath 'results/plots/' --target 'cauchy' --reps 30 --tol 0.0 --lbvi_smc --smc 'smc' --smc_eps 0.05 --smc_sd 1 --smc_T 50 --ubvi --bvi
 
 $SHELL
