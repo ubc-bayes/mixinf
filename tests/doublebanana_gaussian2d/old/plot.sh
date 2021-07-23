0ec89f404b4e2ce62bfe16073ebcecf1d307e50c
GiankDiluvi <giankdiluvi@gmail.com>
2021-07-23 09:40:11 -0700

    rm old banana results

--- tests/doublebanana_gaussian2d/old/plot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -W ignore ../../bivariate-plot.py --inpath 'results/' --outpath 'results/plots/' --target 'double-banana-gaussian' --reps 20 --tol 0.1 0.05 0.01 0.005 0.001 --no_dens --lbvi --kernel 'gaussian' --rkhs 'rbf' --ubvi --bvi --gvi --hmc --rwmh
-
-$SHELL
